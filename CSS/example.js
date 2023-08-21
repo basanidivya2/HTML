186741f0a10e735ee707b69ab6dbc55318e5af7c
Divya <basanidivya2@gmail.com>
2023-08-21 23:51:31 +0530

    added example.js

---
+++ CSS/example.js
@@ -0,0 +1,68 @@
+console.log('Divya');
+
+//let x = 56;
+//let y = 42;
+//console.log(x+y);
+
+//let a = 7;
+//console.log('a value before', a);
+
+//let a = 10;
+//console.log('a value after', a);
+
+//let a = 7;
+//let b = 3;
+//console.log(a*b);
+
+//let a = 25;
+//let b = 35;
+//console.log(a+b);
+
+//let a = 76;
+//let b = 32;
+//console.log(a-b);
+
+//let a = 100;
+//let b = 3;
+//console.log(a/b);
+
+//let a = 4;
+//let b = '54';
+//console.log(a==b);
+
+//let x = 8;
+//let y = 8;
+//console.log(x===y);
+
+//let x = "25";
+//let y = '25';
+//console.log(x===y);
+
+//let a = 32;
+//let b = 45;
+//console.log(a!=b);
+
+//let x = 60;
+//let y = '60';
+//console.log(x!==y);
+
+//let x = 100;
+//let y = 10;
+//console.log(x>y);
+
+//let a = 99;
+//let b = 98;
+//console.log(a<b);
+
+//let a = 55;
+//let b = 65;
+//console.log(a<=b);
+
+let a = 100;
+let b = 3;
+console.log(a++b);
+
+
+
+
+
